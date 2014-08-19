8beaa6c59c8e4913a277c232355718d2f48d2ec0
Phillp Popp <ppopp@gracenote.com>
2014-08-18 17:16:24 -0700

    added body to setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='genepy',
+    version='0.1',
+    description='Genetic Algorithm Framework',
+    author='Phillip Popp, Eric Battenberg',
+    author_email='ppopp@gracenote.com, ebattenberg@gracenote.com',
+    url='http://gitlab.gracenote.gracenote.com/ppopp/genepy',
+    packages=['genepy']
+)
