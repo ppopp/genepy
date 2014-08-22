8bb19e979d650620a762e02bb33b4f999026bf64
ppopp <ppopp@ppopp-bamf.(none)>
2014-08-22 16:33:34 -0700

    added documentation and multiprocessing support

--- genepy/ga.py
+++ genepy/ga.py
@@ -1,11 +1,126 @@
+'''
+This module contains methods for performing heuristic searches using a genetic 
+algorithm. It exposes control over how gene's are mutated and crossed, as well 
+as how the next generation population is selected.  Additionally, it offers two 
+callbacks, one to calculate the fitness of a set of genes, and a second 
+triggered after each generation to allow for stoping conditions, logging, and 
+other functionality.
+
+Setting up and controlling the algorithm is performed through 3 basic 
+mechanisms, the "gene_properties", the "get_fitness" callback and the 
+"on_generation" callback.
+
+The "on_generation" callback is called after each new generation is created and 
+their fitness calculated. If the "on_generation" callback returns anything 
+other than None, the search is halted. It has the following signature:
+
+    def on_generation(
+        iterations, 
+        individuals, 
+        genepool, 
+        gene_properties, 
+        fitness, 
+        **kwargs):
+
+    iterations      - The number of generations thus far.
+
+    individuals     - A list of individuals in the population. By default, 
+    individuals are created by calling the str(..) method on a dictionary of 
+    genes. This can be altered by setting the gene_hash_function in the 
+    genepy.population module.
+
+    genepool        - A dictionary between an individual in the population and 
+    their corresponding genes.  The genes in a genepool are also dictionaries 
+    mapping between an individuals gene name and gene value.  
+    
+    For example:
+    {
+        'individual1': {
+            'gene1': 0.5,
+            'gene2': 0.3
+        },
+        'individual2': {
+            'gene1': 0.8,
+            'gene2': 0.1
+        },
+    }
+
+    gene_properties - The properties used to evelove the population. Further 
+    documentation is available in the genepy.population module.
+
+    fitness         - A dictionary mapping individuals to their fitness scores.
+
+The "get_fitness" callback is called with the genes of an individual.  It's 
+important to note that the fitness results of identical genes are cached for 
+the entirety of the search. The "get_fitness" callback must accept a single 
+argument which is the genes for an individual, and return a fitness score. 
+Higher scores are interpreted as denoting that the individual is more fit.  
+
+    def fitness(genes):
+        ...
+        calculate fitness score
+        ...
+        return score
+
+    genes - A dictionary containing gene values.
+
+The "gene_properties" provide functions to generated, mutate, and crossover 
+gene values.  In addition, they allow a general post processing function that
+is applied any time a new gene value is created.  This can be useful if you
+want to things such as quantize the values or ensure they are within a
+particular range.  The fields of gene_properties are all optional, with default 
+functionality useful for gene values which generally live in the range 
+(0.0, 1.0).
+
+"gene_properties" support the following fields:
+    "generate"  - A python generator which yields initial values for genes
+
+    "mutate"    - A function which accepts the previous gene value and produces
+    a new gene value.
+
+    "crossover" - A function which accepts two gene values and returns a tuple
+    containing two new values.  The order of the individuals which produced the
+    gene values is maintained during assignment. If you wanted to swap the gene
+    values between the two individuals, you would provide a function such as:
+    def swap(a, b):
+        return (b, a)
+
+    "process"   - A function which accepts a single argument and returns a 
+    single argument.  The "process" function is applied whenever a new gene
+    value is created through the "generate", "mutate", or "crossover" 
+    functions.
+
+    Example "gene_properties":
+
+    def uniform_generator(min, max):
+        while True:
+            yield random.uniform(min, max)
+
+    {
+        'gene1': {
+            'generate': uniform_generator(-5.0, 5.0),
+            'mutate': lambda x: return x * random.random(),
+            'crossover': lambda x,y: return (y, x),
+            'process': lambda x: round(x, 2)
+        },
+        'gene2': {
+            ...
+        }
+        ...
+    }
+'''
 import operator
 import logging
+import multiprocessing
 
 import numpy
 
 import population
 
 _logger = logging.getLogger(__name__)
+
+class Error(Exception):
+    pass
 
 def _fitness_of_population(fitness, population=None):
     results_in_population = None
@@ -56,6 +171,13 @@ def best_ratio_convergence(
     gene_properties, 
     fitness, 
     **kwargs):
+    '''
+    Tests for convergence of best_ratio below a threshold.
+
+    [optional]
+    best_ratio_thresh - The upper threshold for convergence.
+    [Default: 0.001]
+    '''
 
     br = best_ratio(fitness)
     thresh = kwargs.get('best_ratio_thresh', 0.001)
@@ -80,6 +202,13 @@ def max_iteration_convergence(
     gene_properties, 
     fitness, 
     **kwargs):
+    '''
+    Tests for convergence of maximum iterations.'
+
+    [optional]
+    max_iterations - The maximum number of iterations
+    [Default: 1000]
+    '''
 
     max_iterations = kwargs.get('max_iterations', 1000)
     _logger.debug(
@@ -94,6 +223,22 @@ def max_iteration_convergence(
         return genepool[result]
 
 def create_generation_callstack(callstack):
+    '''
+    Creates a function which calls all the on_generation functions supplied in
+    the callstack. If a function returns a value other than None, the 
+    subsequent functions will not be called.
+
+    callstack - A list of functions with the on_generation signature.
+
+        def on_generation(
+            iterations, 
+            individuals, 
+            genepool, 
+            gene_properties, 
+            fitness, 
+            **kwargs)
+    '''
+
     def _on_generation(
         iterations, 
         individuals, 
@@ -115,29 +260,113 @@ def create_generation_callstack(callstack):
         return result
     return _on_generation 
 
-def search(population_size, gene_properties, get_fitness, on_generation, **kwargs):
+def _get_fitness(get_fitness, genepool, global_fitness):
+    # get fitness using current process
+    fitness = {}
+    for individual, genes in genepool.items():
+        if not (individual in global_fitness):
+            fit = get_fitness(genes)
+            fitness[individual] = fit
+            global_fitness[individual] = fit
+        else:
+            fitness[individual] = global_fitness[individual]
+    return fitness
 
+def _update_fitness_multiprocess(
+    get_fitness, 
+    genepool, 
+    global_fitness, 
+    processor_count, 
+    timeout, 
+    timeout_fitness):
+
+    # get fitness using a multiprocessor pool
+    fitness = {}
+    pool = None
+    try:
+        pool = multiprocessing.Pool(processor_count)
+        async_fitness = {}
+        for individual, genes in genepool.items():
+            if not (individual in global_fitness):
+                async_fitness[individual] = pool.apply_async(get_fitness, [genes])
+            else:
+                fitness[individual] = global_fitness[individual]
+        pool.close()
+        for individual, async_result in async_fitness.items():
+            try:
+                fitness[individual] = async_result.get(timeout=timeout)
+            except multiprocessing.TimeoutError, e:
+                _logger.warning(
+                    'fitness function timeout after {0} seconds with genes {1}'.format(
+                        timeout,
+                        genepool[individual]))
+                if not (timeout_fitness) is None:
+                    fitness[individual] = timeout_fitness
+                else:
+                    raise
+    except:
+        if pool:
+            pool.terminate()
+        raise
+    finally:
+        if pool:
+            pool.join()
+    return fitness
+
+def search(population_size, gene_properties, get_fitness, on_generation, **kwargs):
+    '''
+    processor_count
+    [Default: 1]
+
+    timeout
+    [Default: None]
+
+    timeout_fitness
+    [Default: None]
+    '''
+
+    # get optional parameters
+    processor_count = kwargs.get('processor_count', 1)
+    _logger.debug('setting processor count to {0}'.format(processor_count))
+    timeout = kwargs.get('timeout', None)
+    _logger.debug('setting timeout to {0}'.format(timeout))
+    timeout_fitness = kwargs.get('timeout_fitness', None)
+    if (timeout is None) and processor_count > 1:
+        _logger.warning('timeout not applied when using single process')
+
+
+    # create initial generation
     _logger.info('creating initial population')
     individuals, genepool = population.create(
         population_size, 
         gene_properties, 
         **kwargs)
 
+    # search until result is found
     global_fitness = {}
     result = None
     iteration = 0 
     while result is None:
-        fitness = {}
+        fitness = None
+        tests = []
+
 
         _logger.info('measuring fitness of iteration {0}'.format(iteration))
-        for individual, genes in genepool.items():
-            if not (individual in global_fitness):
-                fit = get_fitness(genes)
-                fitness[individual] = fit
-                global_fitness[individual] = fit
-            else:
-                fitness[individual] = global_fitness[individual]
-            _logger.info('fitness {0} for individual {1}'.format(fitness[individual], individual))
+        if processor_count > 1:
+            fitness = _update_fitness_multiprocess(
+                get_fitness, 
+                genepool, 
+                global_fitness, 
+                processor_count, 
+                timeout, 
+                timeout_fitness)
+        else:
+            fitness = _get_fitness(get_fitness, genepool, global_fitness)
+
+        for individual in individuals:
+            _logger.info('fitness {0} for individual {1}'.format(
+                fitness[individual], 
+                individual))
 
         result = on_generation(
             iteration, 
--- genepy/test/test_ga.py
+++ genepy/test/test_ga.py
@@ -3,6 +3,7 @@ import numpy
 import random
 import logging
 import pprint
+import time
 
 from genepy import ga
 from genepy import generate
@@ -11,9 +12,20 @@ from genepy import crossover
 
 _logger = logging.getLogger(__name__)
 
-def test_search():
 
 
+def _create_fitness(target):
+    def _fitness(genes):
+        a_error = math.fabs(genes['a'] - target['a']) / target['a_range']
+        b_error = math.fabs(genes['b'] - target['b']) / target['b_range']
+        return 1.0 - (a_error + b_error)
+    return _fitness
+
+def _fitness(target, genes):
+    f = _create_fitness(target)
+    return f(genes)
+
+def _run_search_with_kwargs(kwargs):
     # just try to guess two numbers within some distance
     population_size = 51
     gene_parameters = {
@@ -34,19 +46,6 @@ def test_search():
         'a_range': 20.0,
         'b_range': 200.0
     }
-
-    def _fitness(genes):
-        a_error = math.fabs(genes['a'] - target['a']) / target['a_range']
-        b_error = math.fabs(genes['b'] - target['b']) / target['b_range']
-        return 1.0 - (a_error + b_error)
-
-    kwargs = {
-        'max_iterations': 50,
-        'best_ratio_thresh': 0.01,
-        'active_genes': ['a', 'b'],
-        'mixing_ratio': 0.5,
-        'num_replaces': 1
-    }
     def _update_mutation(
         iterations, 
         individuals, 
@@ -55,44 +54,90 @@ def test_search():
         fitness, 
         **kwargs):
 
-        for gene in kwargs['active_genes']:
+        for gene in gene_properties.keys():
             gene_parameters[gene]['mutate'] = mutate.population_gaussian(
                 individuals, 
                 genepool, 
                 gene)
 
-    def _print_population(
-        iterations, 
-        individuals, 
-        genepool, 
-        gene_properties, 
-        fitness, 
-        **kwargs):
-
-        pprint.pprint(individuals)
-
-
     result = ga.search(
         population_size,
         gene_parameters,
-        _fitness,
+        _create_fitness(target),
         ga.create_generation_callstack([
             ga.max_iteration_convergence,
             ga.best_ratio_convergence,
             _update_mutation,
-            _print_population
+        ]),
+        **kwargs)
+    return (target, result)
+
+
+def test_search():
+    kwargs = {
+        'max_iterations': 50,
+        'best_ratio_thresh': 0.001,
+        'active_genes': ['a', 'b'],
+        'mixing_ratio': 0.5,
+        'num_replaces': 1
+    }
+    target, result = _run_search_with_kwargs(kwargs)
+    assert(_fitness(target, result) >= 0.99)
+
+    kwargs = {
+        'max_iterations': 100,
+        'best_ratio_thresh': 0.001,
+        'mixing_ratio': 0.4,
+        'num_replaces': 3
+    }
+    target, result = _run_search_with_kwargs(kwargs)
+    assert(_fitness(target, result) >= 0.99)
+
+call_count = 0
+def test_fitness_cache():
+    global call_count
+
+    def _gen():
+        x = 1
+        while True:
+            x = x + 1
+            if x > 5:
+                x = 1
+            yield x
+
+    call_count = 0
+    def _call_count_fitness(genes):
+        global call_count
+        call_count += 1
+        return genes['a']
+
+    gene_props = {
+        'a': {
+            'generate': _gen(),
+            'mutate': lambda x: x,
+            'crossover': crossover.swap
+        }
+    }
+
+    kwargs = {
+        'max_iterations': 10,
+    }
+
+
+    result = ga.search(
+        500,
+        gene_props,
+        _call_count_fitness,
+        ga.create_generation_callstack([
+            ga.max_iteration_convergence,
         ]),
         **kwargs)
 
-    assert(not result is None)
-    
-    _logger.info('result {0}'.format(result))
-    _logger.info('target {0}'.format(target))
-    _logger.info('fitness of result {0}'.format(_fitness(result)))
-    assert(_fitness(result) >= 0.99)
+    assert(call_count == 5)
+    assert(not (result is None))
+    assert(result['a'] == 5)
 
 
 
-    
 
 
